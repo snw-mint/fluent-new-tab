0307fab70435fea6c851fe58e3d3931fa0376f00
Andry <andry.we@proton.me>
2026-03-31 15:44:49 -0300

    feat: Kagi search engine added. note: you must log in to the Kagi platform to use the search engine. fix #47

--- src/core/config.ts
+++ src/core/config.ts
@@ -10,7 +10,8 @@ const engines: Record<string, EngineConfig> = {
     brave: { url: 'https://search.brave.com/search', icon: 'assets/search-engines/brave.svg' },
     duck: { url: 'https://duckduckgo.com/', icon: 'assets/search-engines/ddg.svg' },
     ecosia: { url: 'https://www.ecosia.org/search', icon: 'assets/search-engines/ecosia.svg' },
-    startpage: { url: 'https://www.startpage.com/sp/search', icon: 'assets/search-engines/startpg.svg' }
+    startpage: { url: 'https://www.startpage.com/sp/search', icon: 'assets/search-engines/startpg.svg' },
+    kagi: { url: "https://kagi.com/search?q=", icon: "assets/search-engines/kagi.svg" }
 };
 
 const launcherData: Record<string, LauncherProviderData> = {
